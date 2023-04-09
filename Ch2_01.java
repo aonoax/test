9207359291e7ad46b022716e4ca488579832b2b2
A-ON <122158180+aonoax@users.noreply.github.com>
2023-04-09 21:14:20 +0900

    Add files via upload

---
+++ Ch2_01.java
@@ -0,0 +1,20 @@
+package chpt_02;
+
+public class Ch2_01 {
+
+	public static void main(String[] args) {
+		// 변수의 초기화
+		int year = 0;
+		int age = 14;
+		
+		System.out.println("1.year = " + year);
+		System.out.println("1.age = " + age);
+		
+		year = age + 2000;
+		age = age + 1;
+		
+		System.out.println("2.year = " + year);
+		System.out.println("2.age = " + age);
+	}
+
+}
